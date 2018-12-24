7fa6c51d6090581f27300eefc8cecb4d8586b568
Conrad Bituin <inbetweenwebdesigns@gmail.com>
2018-12-24 06:19:37 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -52,7 +52,10 @@ class Owner
   def walk_dogs
     @pets[:dogs].map { |dog| dog.mood = "happy" }
   end
-    
+  
+  def play_with_cats
+    @pets[:cats].map { |cat| dog.mood = "happy" }
+  end 
   
   
 end
